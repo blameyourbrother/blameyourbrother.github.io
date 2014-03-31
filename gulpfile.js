e0ecb37c85369bef81e69984ae86e3bc95d88c65
Chris Mendis <chris.mendis@gmail.com>
2014-03-30 20:28:44 -0400

    Set up template compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,23 @@
 var gulp = require('gulp');
 var clean = require('gulp-clean');
-var handlebars = require('handlebars');
-var compileHandlebars = require('gulp-compile-handlebars');
+var rename = require('gulp-rename');
 var stylus = require('gulp-stylus');
+var build = require('gulp-build');
 var plumber = require('gulp-plumber');
 var path = require('path');
+var fs = require('fs');
+var partialsConfig = [];
 
-gulp.task('default', ['clean', 'styles'], function () {
+gulp.task('default', ['clean', 'styles', 'buildPartialsConfig', 'html'], function () {
   gulp.watch('./src/styles/**/*.styl', ['styles']);
+  gulp.watch('./src/partials/*.hbs', ['buildPartialsConfig', 'html']);
+  gulp.watch('./src/templates/**/*.hbs', ['html']);
 });
 
 gulp.task('clean', function () {
   gulp.src('./assets/css', {read: false})
+    .pipe(clean());
+  gulp.src(['./**/*.html', '!/assets/**/*.html', '!/src/**/*.html', '!/bower_components/**/*.html', '!/node_modules/**/*.html'])
     .pipe(clean());
 });
 
@@ -23,4 +29,30 @@ gulp.task('styles', function () {
       set: ['compress', 'linenos']
     }))
     .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('html', function () {
+  var data = require('./src/data.json');
+  var options = {
+    partials: partialsConfig
+  };
+
+  gulp.src('./src/templates/**/*.hbs')
+    .pipe(plumber())
+    .pipe(build(data, options))
+    .pipe(rename({
+      extname: '.html'
+    }))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('buildPartialsConfig', function () {
+  var partialsList = fs.readdirSync(path.resolve(__dirname, 'src/partials'));
+  for (var i = 0, j = partialsList.length; i < j; i++) {
+    var filename = partialsList[i];
+    partialsConfig.push({
+      name: path.basename(filename, '.hbs'),
+      tpl: fs.readFileSync(path.join(__dirname, 'src/partials', filename), {encoding: 'utf8'})
+    });
+  }
 });
