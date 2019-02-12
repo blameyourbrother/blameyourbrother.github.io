9e37cbe0d35a8a92adf9268fb0b9d2a3d1966a75
Chris Mendis <152701+chrismendis@users.noreply.github.com>
2019-02-11 20:53:04 -0500

    Fix carousel image sizing

--- assets/javascript/work.js
+++ assets/javascript/work.js
@@ -1,14 +1,14 @@
-$(document).ready(function () {
-  $('.work-gallery').on('click', '.gallery-cta', function (ev) {
+$(document).ready(function() {
+  $(".work-gallery").on("click", ".gallery-cta", function(ev) {
     ev.stopPropagation();
     ev.preventDefault();
 
-    var galleryIndex = $(ev.currentTarget).data('gallery-index');
-    var backdrop = $('.work-slideshow-backdrop').show();
-    var $slideshow = $('.work-slideshow');
-    var init = function () {
-      backdrop.css('opacity', 1);
-      var attemptSlickGoTo = window.setInterval(function () {
+    var galleryIndex = $(ev.currentTarget).data("gallery-index");
+    var backdrop = $(".work-slideshow-backdrop").show();
+    var $slideshow = $(".work-slideshow");
+    var init = function() {
+      backdrop.css("opacity", 1);
+      var attemptSlickGoTo = window.setInterval(function() {
         try {
           $slideshow.slickGoTo(galleryIndex);
           window.clearInterval(attemptSlickGoTo);
@@ -19,20 +19,23 @@ $(document).ready(function () {
     };
     $slideshow.slick({
       onInit: init,
-      onReInit: init
+      onReInit: init,
+      variableWidth: true
     });
   });
 
-  $('.work-slideshow-backdrop').on('click', function (ev) {
+  $(".work-slideshow-backdrop").on("click", function(ev) {
     var $target = $(ev.target);
-    if (!$target.hasClass('work-slideshow-backdrop')) {
+    if (!$target.hasClass("work-slideshow-backdrop")) {
       return;
     }
-    $target.hide().css('opacity', 0);
-    $('.work-slideshow').unslick();
+    $target.hide().css("opacity", 0);
+    $(".work-slideshow").unslick();
   });
 
-  $('.work-credits-toggle, .work-credits h4').on('click', function (ev) {
-    $(ev.target).closest('.work-credits').toggleClass('open');
+  $(".work-credits-toggle, .work-credits h4").on("click", function(ev) {
+    $(ev.target)
+      .closest(".work-credits")
+      .toggleClass("open");
   });
 });
